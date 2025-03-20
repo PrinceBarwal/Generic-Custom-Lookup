9c5b0b19fd84847930b16d6e1eb7ff95e3ccea85
Prince Saini <princesaini0144@gmail.com>
2025-03-20 22:37:14 +0530

    Adding the debouncing function for the input

--- force-app/main/default/lwc/customLookup/customLookup.js
+++ force-app/main/default/lwc/customLookup/customLookup.js
@@ -1,12 +1,13 @@
 import { LightningElement, wire } from 'lwc';
 import customLookupController from '@salesforce/apex/customLookupController.getsObjectRecords';
-
+const DELAY = 300;
 export default class CustomLookup extends LightningElement {
 
     apiName = "Account";
     searchValue = "Test";
     objectLabel  = 'Account';
     iconName = 'standard:account';
+    delayTimeOut;
 
     @wire(customLookupController, 
         {
@@ -18,4 +19,12 @@ export default class CustomLookup extends LightningElement {
     // get showOutput(){
     //     return this.outputs.data ? true : false;
     // }
+
+    changeHandler(event){
+        window.clearTimeout(this.delayTimeOut);
+        let enteredValue = event.target.value;
+        this.delayTimeOut = setTimeout(() => {
+            this.searchValue = enteredValue;
+        } , DELAY);
+    }
 }
